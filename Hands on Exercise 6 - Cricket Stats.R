3908c834ccd61183ed6828e5af1746d86d7d2894
Karthik Rao <karthik16@micamail.in>
2017-09-06 15:54:13 +0530

    Highest number of employees identified

--- Hands on Exercise 6 - Cricket Stats.R
+++ Hands on Exercise 6 - Cricket Stats.R
@@ -1,9 +1,9 @@
 #Exercise 6
-#Get data from http://stats.espncricinfo.com/ci/content/records/284248.html and work on the questions
+# 1. Get data from http://stats.espncricinfo.com/ci/content/records/284248.html and work on the questions
 #
-#Plot 5 highest runs across the year
-#Find highest number of times a player had become the highest run scorer in a calendar year
-#Plot contribution of player country in becoming highest run getters EX: Australian players 20%
+# Plot 5 highest runs across the year
+# 2. Find highest number of times a player had become the highest run scorer in a calendar year
+# 3. Plot contribution of player country in becoming highest run getters EX: Australian players 20%
 #
 
 library("xml2")
@@ -23,7 +23,51 @@ View(cricket_table.df)
 Top_Five_Scorers<-c(cricket_table.df$Player[1], cricket_table.df$Player[2], cricket_table.df$Player[3], cricket_table.df$Player[4], cricket_table.df$Player[5])
 Top_Five_Scorers
 
+help(plot)
+Top.Five.df <- NULL
+Top.Five.df$Runs<- cricket_table.df$Runs[1:5]
+Top.Five.df$Year<- cricket_table.df$Year[1:5]
+
+plot(Top.Five.df$Year, Top.Five.df$Runs)
+
 ## Top Five Scorers End
+
+## Number of times a player had become highest run getter in a calendar year
+
+Year_store <- unique(cricket_table.df$Year)
+countOfYears <- rep(0,length(Year_store))
+countOfRuns <- NULL
+Name_array <- NULL
+
+cricket_table_yearsort.df<- cricket_table.df[with(cricket_table.df, order(Year, Player)), ]
+View(cricket_table_yearsort.df)
+
+for (i3 in 1:length(Year_store))
+{
+  if (!(is.na(Year_store[i3])))
+  {
+    for (i4 in 1:nrow(cricket_table_yearsort.df))
+    {
+      if (!(is.na(cricket_table_yearsort.df$Runs[i4])))
+      {
+        if (cricket_table_yearsort.df$Year[i4]==Year_store[i3])
+        {
+          countOfRuns[i3] <- cricket_table_yearsort.df$Runs[i4]
+          Name_array[i3] <- cricket_table_yearsort.df$Player[i4]
+        }  
+        
+      }  
+      
+    }
+  }  
+}
+
+countOfRuns
+Name_array
+
+## End of Number of times a player had become highest run getter in a calendar year
+
+##Start of Contribution ofplayer country in becoming highest run getters
 
 PAK=0
 AUS=0
@@ -95,3 +139,4 @@ ENG_ratio = 100*ENG/Total_country_appearances
 SA_ratio = 100*SA/Total_country_appearances
 
 
+## End of Contribution ofplayer country in becoming highest run getters
